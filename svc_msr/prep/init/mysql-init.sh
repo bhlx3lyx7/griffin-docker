bf7a573c3c094d03062633cf80c12fe5b95c675d
Lionel, Liu <bhlx3lyx7@163.com>
2018-02-26 21:59:14 -0700

    mysql-init

--- svc_msr/prep/init/mysql-init.sh
+++ svc_msr/prep/init/mysql-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-service mysql start
+find /var/lib/mysql -type f -exec touch {} \; && service mysql start
 
 mysql -u root < quartz-init.sql
 mysql -u griffin -p123456 -D quartz < quartz-table-init.sql
